1a2d5c451a3d66507c1b04f260fe4ba484d34e7f
Miller Denis <8264513+dmllr@users.noreply.github.com>
2017-10-27 17:45:44 +0900

    Platform tests

---
+++ erubit.platform/src/main/java/a/erubit/platform/tests/Tests.java
@@ -0,0 +1,9 @@
+package a.erubit.platform.tests;
+
+import a.erubit.platform.course.CourseManager;
+
+public class Tests {
+    public static void _contents() {
+        CourseManager.i().initialize();
+    }
+}
